8caad10e4e11e47ed84c8161c2bf015c727f7e5c
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-07 23:36:20 +0200

    Improve backend, seems quite ready.

--- Marf.py
+++ Marf.py
@@ -2,20 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import os
+import subprocess
 
 class MarfSpeaker(object):
-    def __init__(self, marf_path, s_id, s_name, s_training, s_testing):
+    def __init__(self, s_id, s_name, s_training, s_testing):
         self.s_id = s_id
         self.s_name = s_name
         self.s_training = s_training
         self.s_testing = s_testing
 
 
-
 class Marf(object):
-    def __init__(self, marf_path="/opt/marf"):
+    def __init__(self, marf_path="marf"):
         self.marf_path = marf_path
         self.speaker_db = os.path.join(self.marf_path, "speakers.txt")
+
+        # Should always be run in the marf directory
+        self.cmd = ["java", "-ea", "-Xmx512m", "-jar", "SpeakerIdentApp.jar"]
 
         self.speakers = {}
 
@@ -23,50 +26,98 @@ class Marf(object):
         for line in open(self.speaker_db, "r").readlines():
             data = line.strip().split(",")
 
-            s_id = int(data[0].strip())
-            s_name = data[1].strip()
             s_training = []
             s_testing = ""
+
+            s_id = int(data.pop(0))
+            s_name = data.pop(0)
             try:
-                # s_training is a list of wav files joined with '|'
-                s_training = data[2].split("|")
+                s_training = data.pop(0).split("|")
+                s_testing = data.pop(0)
             except IndexError:
                 pass
 
-            try:
-                s_testing = data[3].strip()
-            except IndexError:
-                pass
+            self.speakers[s_id] = MarfSpeaker(s_id, s_name, s_training, s_testing)
 
-            self.speakers[s_id] = [s_name, s_training, s_testing]
+        # Hold the next id
+        self.next_id = max(self.speakers.keys()) + 1
 
-    def dump_speakers(self):
-        for sp_id in sorted(self.speakers.keys()):
-            sp_data = self.speakers[sp_id]
-            sp = "%s,%s" % (sp_id, sp_data[0])
-            try:
-                data = "%s" % "|".join(sp_data[1])
-            except IndexError:
-                pass
-            else:
-                sp += ",%s" % data
+    def get_all_speakers(self):
+        return self.speakers
 
-            try:
-                data = sp_data[2]
-            except IndexError:
-                pass
-            else:
-                sp += ",%s" % data
+    def get_trained_speakers(self):
+        d =  {}
+        for key, value in self.speakers.items():
+            if value.s_training:
+                d[key] = value
 
-            print sp
+        return d
 
-    def add_speaker(self, s_id, s_name, s_training, s_testing):
-        pass
+    def train(self):
+        train_cmd = self.cmd[:]
+        train_cmd.extend(["--train", "training-samples",
+                          "-endp", "-lpc", "-cheb"])
+        process = subprocess.Popen(train_cmd,
+                                   cwd=self.marf_path,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        return process.communicate()[0].startswith("Done training on folder \"training-samples\".")
+
+
+    def identify(self, sample):
+        # sample is the temporarily recorded wave file
+        ident_cmd = self.cmd[:]
+        ident_cmd.extend(["--ident", "testing-samples/%s" % sample,
+                          "-endp", "-lpc", "-cheb"])
+        process = subprocess.Popen(ident_cmd,
+                                   cwd=self.marf_path,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        stdout = process.communicate()[0]
+        for line in stdout.split("\n"):
+            if "Expected Speaker's ID:" in line.strip():
+                return line.split("Expected Speaker's ID:")[1].strip()
+
+    def write_speakers(self):
+        speaker_file = open(self.speaker_db, "w")
+
+        for s_id in sorted(self.speakers.keys()):
+            speaker = self.speakers[s_id]
+            speaker_file.write("%s,%s" % (speaker.s_id, speaker.s_name))
+            if speaker.s_training:
+                speaker_file.write(",%s" % ("|".join(speaker.s_training)))
+                if speaker.s_testing:
+                    speaker_file.write(",%s" % speaker.s_testing)
+            speaker_file.write("\n")
+
+    def add_speaker(self, s_name, s_training, s_testing):
+        self.speakers[self.next_id] = MarfSpeaker(self.next_id,
+                                                  s_name,
+                                                  s_training,
+                                                  s_testing)
+
+        self.write_speakers()
+        self.next_id += 1
 
 
 
 if __name__ == "__main__":
 
     m = Marf()
-    m.dump_speakers()
+    #m.add_speaker("Ozan", ["ozan1.wav", "ozan2.wav"], "ozan-test.wav")
+    """
+    print "Training: %s" % m.train()
 
+    # Random identify
+    import math
+    import time
+    import random
+
+    random.seed(time.time())
+
+    trained_speakers = m.get_trained_speakers()
+    random_id = int(math.floor(random.random()*len(trained_speakers)))
+    speaker = trained_speakers[random_id]
+    print "Identifying (%d) %s" % (speaker.s_id, speaker.s_name)
+    print "ID Identified: %s" % m.identify(speaker.s_testing)
+    """
--- install-marf.sh
+++ install-marf.sh
@@ -2,8 +2,9 @@
 
 # Download marf bundle
 wget -nc -P /tmp "http://downloads.sourceforge.net/project/marf/Applications/%5Bf%5D%20SpeakerIdentApp/0.3.0-devel-20060226/SpeakerIdentApp-bundle-0.3.0-devel-20060226.tar.bz2"
-mkdir -p /opt/marf &> /dev/null
-tar xvf /tmp/SpeakerIdentApp-bundle-0.3.0-devel-20060226.tar.bz2 -C /opt/marf
-chmod 777 /opt/marf/{training-samples,testing-samples,speakers.txt}
-cd /opt/marf
+mkdir -p marf &> /dev/null
+tar xvf /tmp/SpeakerIdentApp-bundle-0.3.0-devel-20060226.tar.bz2 -C marf
+chmod 755 marf
+chown -R $USER: marf
+cd marf
 rm -rf html *.html retrain.lnk testing.bat
